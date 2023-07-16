b5a03b0cbc957f96fd9a4abfa2fe9b94dfa0f71a
Vanya <vanyailnitsk@yandex.ru>
2023-07-16 22:02:40 +0300

    First commit

---
+++ src/test/java/com/ilnitsk/animusic/AnimusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilnitsk.animusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
