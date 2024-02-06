a3e531d0d0fa0e2b1f1dbf46e9b6e36abdcc2999
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-02-07 00:38:37 +0300

    fix файла миграции

--- database/migration/V1__add-image-file-to-soundtrack.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE soundtrack RENAME COLUMN path_to_file to audio_file;
-
-ALTER TABLE soundtrack ADD COLUMN image_file character varying(255);
---
+++ database/migration/V2__add-image-file-to-soundtrack.sql
@@ -0,0 +1,3 @@
+ALTER TABLE soundtrack RENAME COLUMN path_to_file to audio_file;
+
+ALTER TABLE soundtrack DROP COLUMN type;
