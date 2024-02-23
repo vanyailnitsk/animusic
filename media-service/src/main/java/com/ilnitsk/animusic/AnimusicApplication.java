74c2c2c78dbe6cd02f197f116608bd345c4f6c5e
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-02-23 18:34:03 +0300

    Eureka discovery and created user-service

--- media-service/src/main/java/com/ilnitsk/animusic/AnimusicApplication.java
+++ media-service/src/main/java/com/ilnitsk/animusic/AnimusicApplication.java
@@ -1,8 +1,8 @@
 package com.ilnitsk.animusic;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -11,6 +11,7 @@ import org.springframework.web.filter.CorsFilter;
 import java.util.Arrays;
 
 @SpringBootApplication
+//@EnableDiscoveryClient
 public class AnimusicApplication {
 
 	public static void main(String[] args) {
