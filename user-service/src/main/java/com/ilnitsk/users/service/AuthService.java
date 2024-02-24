0e9ada60ed071aa327a4691b90e3ce66ca671b1b
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-02-25 00:40:09 +0300

    Созданы JwtService и AuthService

---
+++ user-service/src/main/java/com/ilnitsk/users/service/AuthService.java
@@ -0,0 +1,35 @@
+package com.ilnitsk.users.service;
+
+import com.ilnitsk.users.UserRepository;
+import com.ilnitsk.users.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtService jwtService;
+
+    public String saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return "user added to the system";
+    }
+
+    public String generateToken(String username) {
+        return jwtService.generateToken(username);
+    }
+
+    public void validateToken(String token) {
+        jwtService.validateToken(token);
+    }
+
+
+}
