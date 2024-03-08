c22a63b49c1de997795ded5cd5221800203bbf79
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-03-08 23:17:33 +0300

    Описание TokenDTO в Swagger

--- src/main/java/com/ilnitsk/animusic/security/dto/TokenDto.java
+++ src/main/java/com/ilnitsk/animusic/security/dto/TokenDto.java
@@ -1,5 +1,6 @@
 package com.ilnitsk.animusic.security.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -7,11 +8,8 @@ import lombok.*;
 @Builder
 @RequiredArgsConstructor
 public class TokenDto {
+    @Schema(name = "accessToken",description = "JWT-токен доступа к ресурсам",example = "eyJhbGciOiJIUzI1NiIsInR5cCI6")
     private String accessToken;
+    @Schema(name = "refreshToken",description = "Одноразовый токен для получения нового access-токена",example = "JzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ikpva")
     private String refreshToken;
-
-    public TokenDto(String accessToken, String refreshToken) {
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-    }
 }
