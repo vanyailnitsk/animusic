4575bb43b9add4fab8777f4a4388471c15e9e9ae
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-03-09 16:59:44 +0300

    Внедрены свойства в S3Config

--- src/main/java/com/ilnitsk/animusic/s3/S3Config.java
+++ src/main/java/com/ilnitsk/animusic/s3/S3Config.java
@@ -1,5 +1,8 @@
 package com.ilnitsk.animusic.s3;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
@@ -8,11 +11,18 @@ import software.amazon.awssdk.services.s3.S3Client;
 import java.net.URI;
 
 @Configuration
+@ConfigurationProperties(prefix = "timeweb.s3")
+@RequiredArgsConstructor
+@Data
 public class S3Config {
+
+    private String url;
+    private String bucket;
 
     @Bean
     public S3Client s3Client() {
         S3Client client = S3Client.builder()
+                .endpointOverride(URI.create(url))
                 .region(Region.of("ru-1"))
                 .build();
         return client;
