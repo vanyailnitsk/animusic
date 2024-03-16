3861d8476229f7b2f6433dd3f9d7896084452bd8
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-03-16 13:39:33 +0300

    Методы RefreshTokenService для создания и валидации refresh-токена

--- src/main/java/com/ilnitsk/animusic/security/service/RefreshTokenService.java
+++ src/main/java/com/ilnitsk/animusic/security/service/RefreshTokenService.java
@@ -1,16 +1,43 @@
 package com.ilnitsk.animusic.security.service;
 
+import com.ilnitsk.animusic.exception.TokenRefreshException;
+import com.ilnitsk.animusic.exception.UserNotFoundException;
 import com.ilnitsk.animusic.security.RefreshToken;
 import com.ilnitsk.animusic.security.RefreshTokenRepository;
+import com.ilnitsk.animusic.user.dao.User;
+import com.ilnitsk.animusic.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class RefreshTokenService {
     private final RefreshTokenRepository refreshTokenRepository;
+    private final UserRepository userRepository;
+    @Value("${token.refreshExpirationHours}")
+    private final Integer refreshTTLhours;
 
-    public RefreshToken createRefreshToken() {
+    public RefreshToken createRefreshToken(Integer userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(userId));
+        RefreshToken refreshToken = RefreshToken.builder()
+                .user(user)
+                .expireDate(Instant.now().plusSeconds(refreshTTLhours*60*60))
+                .token(UUID.randomUUID().toString())
+                .build();
+        refreshTokenRepository.save(refreshToken);
+        return refreshToken;
+    }
 
+    public RefreshToken verifyExpiration(RefreshToken refreshToken) {
+        if (refreshToken.getExpireDate().compareTo(Instant.now()) < 0) {
+            refreshTokenRepository.delete(refreshToken);
+            throw new TokenRefreshException(refreshToken.getToken(), "Refresh token expired");
+        }
+        return refreshToken;
     }
 }
