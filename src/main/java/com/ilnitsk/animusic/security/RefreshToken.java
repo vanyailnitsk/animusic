ae414b11cc6469e542f7667714b9aa76f883c92c
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-03-16 13:38:55 +0300

    Изменен класс RefreshToken

--- src/main/java/com/ilnitsk/animusic/security/RefreshToken.java
+++ src/main/java/com/ilnitsk/animusic/security/RefreshToken.java
@@ -1,20 +1,32 @@
 package com.ilnitsk.animusic.security;
 
+import com.ilnitsk.animusic.user.dao.User;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+
+import java.time.Instant;
 
 @Entity
 @Table
 @RequiredArgsConstructor
 @Data
+@Builder
 public class RefreshToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String value;
 
-    public RefreshToken(String value) {
-        this.value = value;
-    }
+    @Column(nullable = false,unique = true)
+    private String token;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private Instant expireDate;
+
+
 }
