447a3d4be41f3ee28dadaa59e439792a4d55c79e
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-03-26 12:37:18 +0300

    Описание PlaylistController в Swagger

--- src/main/java/com/ilnitsk/animusic/playlist/PlaylistController.java
+++ src/main/java/com/ilnitsk/animusic/playlist/PlaylistController.java
@@ -4,6 +4,10 @@ import com.ilnitsk.animusic.anime.AnimeRepository;
 import com.ilnitsk.animusic.playlist.dto.CreatePlaylistRequest;
 import com.ilnitsk.animusic.playlist.dto.PlaylistConverter;
 import com.ilnitsk.animusic.playlist.dto.PlaylistDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,11 +20,18 @@ import java.util.List;
 @RequestMapping("/api/playlist")
 @Slf4j
 @RequiredArgsConstructor
+@Tag(name = "REST API для управления альбомами", description = "Предоставляет методы для управление альбомами")
 public class PlaylistController {
     private final PlaylistService playlistService;
     private final AnimeRepository animeRepository;
     private final PlaylistConverter playlistConverter;
     @GetMapping("/by-anime/{animeId}")
+    @Operation(summary = "Метод для получения списка альбомов по animeId")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Успешное получение альбомов."),
+            @ApiResponse(responseCode = "404", description = "Альбом не найден не найдено"),
+            @ApiResponse(responseCode = "500", description = "Ошибка на стороне сервера")
+    })
     public List<PlaylistDto> getPlaylistsByAnime(@PathVariable Integer animeId) {
         log.info("Requested playlists by anime {}", animeId);
         List<Playlist> playlists = playlistService.getPlaylistsByAnimeId(animeId);
@@ -30,6 +41,12 @@ public class PlaylistController {
     }
 
     @GetMapping("{id}")
+    @Operation(summary = "Метод для получения альбома по id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Успешное получение альбома"),
+            @ApiResponse(responseCode = "404", description = "Альбом не найден"),
+            @ApiResponse(responseCode = "500", description = "Ошибка на стороне сервера")
+    })
     public PlaylistDto getPlaylistById(@PathVariable Integer id, HttpServletRequest request) {
         log.info("Requested playlist with id {}", id);
         Playlist playlist = playlistService.getPlaylistById(id);
@@ -44,6 +61,12 @@ public class PlaylistController {
     }
 
     @PostMapping
+    @Operation(summary = "Метод для создания альбома")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Успешное создание альбома."),
+            @ApiResponse(responseCode = "400", description = "Альбом уже существует"),
+            @ApiResponse(responseCode = "500", description = "Ошибка на стороне сервера")
+    })
     public Playlist createPlaylist(@RequestBody CreatePlaylistRequest request) {
         Playlist playlist = playlistService.createPlaylist(request);
 
@@ -51,6 +74,12 @@ public class PlaylistController {
         return playlist;
     }
     @DeleteMapping("{id}")
+    @Operation(summary = "Удаление альбома")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Успешное удаление альбома."),
+            @ApiResponse(responseCode = "404", description = "Альбом не найден"),
+            @ApiResponse(responseCode = "500", description = "Ошибка на стороне сервера")
+    })
     public void deletePlaylist(@PathVariable Integer id) {
         playlistService.deletePlaylist(id);
         log.info("Playlist with id {} deleted",id);
