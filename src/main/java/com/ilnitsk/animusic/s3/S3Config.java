6da0906da017e8679de1d528d04965b03368d29e
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-03-26 23:12:35 +0300

    configurable s3 properties (for testing/prod)

--- src/main/java/com/ilnitsk/animusic/s3/S3Config.java
+++ src/main/java/com/ilnitsk/animusic/s3/S3Config.java
@@ -2,9 +2,12 @@ package com.ilnitsk.animusic.s3;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -15,15 +18,21 @@ import java.net.URI;
 @RequiredArgsConstructor
 @Data
 public class S3Config {
-
     private String url;
     private String bucket;
+    private String region;
+    @Value("access-key")
+    private String accessKey;
+    @Value("secret-key")
+    private String secretKey;
 
     @Bean
     public S3Client s3Client() {
+        AwsCredentials credentials = AwsBasicCredentials.create(accessKey,secretKey);
         S3Client client = S3Client.builder()
+                .credentialsProvider(() -> credentials)
                 .endpointOverride(URI.create(url))
-                .region(Region.of("ru-1"))
+                .region(Region.of(region))
                 .build();
         return client;
     }
