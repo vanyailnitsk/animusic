297de645030e21b06c2c84a10e073b866dee56cf
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-03-26 13:53:31 +0300

    Описан UserController в Swagger

--- src/main/java/com/ilnitsk/animusic/user/controller/UserController.java
+++ src/main/java/com/ilnitsk/animusic/user/controller/UserController.java
@@ -3,6 +3,9 @@ package com.ilnitsk.animusic.user.controller;
 import com.ilnitsk.animusic.user.dao.User;
 import com.ilnitsk.animusic.user.dto.UserDto;
 import com.ilnitsk.animusic.user.service.UserService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "REST API для управления данными пользователя", description = "Предоставляет методы для управления данными пользователя")
 public class UserController {
     private final UserService userService;
 
     @GetMapping("")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Успешное получение данных пользователя"),
+            @ApiResponse(responseCode = "401", description = "Не авторизован"),
+            @ApiResponse(responseCode = "500", description = "Ошибка на стороне сервера")
+    })
     public UserDto getUser() {
         User user = userService.getUserInSession();
         return new UserDto(user);
