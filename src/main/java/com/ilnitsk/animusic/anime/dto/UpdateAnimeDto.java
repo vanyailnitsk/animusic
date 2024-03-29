6d88a8e997eac27d8179a3d8a67f88178cb31df5
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-03-29 22:25:36 +0300

    Обновлен UpdateAnimeDto

--- src/main/java/com/ilnitsk/animusic/anime/dto/UpdateAnimeDto.java
+++ src/main/java/com/ilnitsk/animusic/anime/dto/UpdateAnimeDto.java
@@ -2,15 +2,11 @@ package com.ilnitsk.animusic.anime.dto;
 
 import lombok.Data;
 
-import java.time.Year;
-
 @Data
 public class UpdateAnimeDto {
     private String title;
     private String studio;
-    private Year releaseYear;
+    private Integer releaseYear;
     private String description;
     private String folderName;
-    private String bannerImagePath;
-    private String cardImagePath;
 }
