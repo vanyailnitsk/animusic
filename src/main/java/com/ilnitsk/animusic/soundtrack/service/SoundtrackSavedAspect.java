1662fa221e83c6e14686e064a5ab6084c214bd85
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-04-02 23:17:58 +0300

    Создан Aspect, который после конвертации трека в SoundtrackEntityDto устанавливает значение полю saved

---
+++ src/main/java/com/ilnitsk/animusic/soundtrack/service/SoundtrackSavedAspect.java
@@ -0,0 +1,55 @@
+package com.ilnitsk.animusic.soundtrack.service;
+
+import com.ilnitsk.animusic.album.dto.AlbumDto;
+import com.ilnitsk.animusic.soundtrack.dto.SoundtrackDto;
+import com.ilnitsk.animusic.soundtrack.dto.SoundtrackEntityDto;
+import com.ilnitsk.animusic.user.dao.User;
+import com.ilnitsk.animusic.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class SoundtrackSavedAspect {
+
+    private final UserService userService;
+
+    @AfterReturning(pointcut = "execution(* com.ilnitsk.animusic.soundtrack.dto.SoundtrackConverter.convertToDto(..))", returning = "dto")
+    public void setSavedField(Object dto) {
+        SoundtrackDto soundtrackDto = (SoundtrackDto) dto;
+        SoundtrackEntityDto entityDto = soundtrackDto.getSoundtrack();
+        Set<Integer> savedTracks = getUserSavedTracksIds();
+        if (savedTracks.contains(entityDto.getId())) {
+            entityDto.setSaved(true);
+        }
+    }
+
+    public Set<Integer> getUserSavedTracksIds() {
+        Optional<User> user = userService.getUserInSession();
+        if (user.isEmpty()) {
+            return Set.of();
+        }
+        Set<Integer> savedTracksIds = user.get().getFavouriteTracks().getSoundtracks()
+                .stream()
+                .map(s -> s.getSoundtrack().getId())
+                .collect(Collectors.toSet());
+        return savedTracksIds;
+    }
+    @AfterReturning(pointcut = "execution(* com.ilnitsk.animusic.album.dto.AlbumConverter.convertToDto(..))", returning = "dto")
+    public void setSavedToAlbum(Object dto) {
+        AlbumDto albumDto = (AlbumDto) dto;
+        Set<Integer> savedTracks = getUserSavedTracksIds();
+        albumDto.getSoundtracks().stream()
+                .map(SoundtrackDto::getSoundtrack)
+                .filter(s -> savedTracks.contains(s.getId()))
+                .forEach(s -> s.setSaved(true));
+    }
+
+}
