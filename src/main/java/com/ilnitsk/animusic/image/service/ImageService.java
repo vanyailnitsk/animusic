f5e8228e31da6ae0acb1d0546817e03d0ecbce99
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-04-04 23:14:42 +0300

    Метод для получения изображения по id

--- src/main/java/com/ilnitsk/animusic/image/service/ImageService.java
+++ src/main/java/com/ilnitsk/animusic/image/service/ImageService.java
@@ -6,6 +6,7 @@ import com.ilnitsk.animusic.s3.S3Service;
 import com.ilnitsk.animusic.soundtrack.dao.Soundtrack;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
@@ -29,12 +30,18 @@ public class ImageService {
         imageRepository.delete(image);
     }
 
+    @Transactional
     public Image createImage(String animeName,String imageName,MultipartFile image) {
         Image imageEntity = new Image();
         String fileName = IMAGES_PATH.formatted(animeName,imageName);
         String blobKey = s3Service.createBlob(fileName,image);
         imageEntity.setSource(blobKey);
         return imageRepository.save(imageEntity);
+    }
+
+    public Image getImage(Integer id) {
+        return imageRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Image not found"));
     }
 
 }
