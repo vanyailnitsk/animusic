5894ccab304d4e70e0b2151a7677cbaf4fb6d7a5
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-04-04 22:47:59 +0300

    Метод для конвертации CreateCoverDto в CoverArt

--- src/main/java/com/ilnitsk/animusic/image/dto/CoverArtConverter.java
+++ src/main/java/com/ilnitsk/animusic/image/dto/CoverArtConverter.java
@@ -14,5 +14,11 @@ public class CoverArtConverter {
         modelMapper.createTypeMap(CoverArt.class, CoverArtDto.class)
                 .addMapping(CoverArt::getColorDark,CoverArtDto::setColorDark)
                 .addMapping(CoverArt::getColorLight,CoverArtDto::setColorLight);
+
+        modelMapper.createTypeMap(CreateCoverDto.class,CoverArt.class);
+    }
+
+    public CoverArt convertToEntity(CreateCoverDto dto) {
+        return modelMapper.map(dto,CoverArt.class);
     }
 }
