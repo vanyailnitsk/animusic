1334a6bba5c06a41865e934cdf6fcfcecf0e2130
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-04-05 22:07:48 +0300

    fix for docker

--- src/main/java/com/ilnitsk/animusic/album/dao/Album.java
+++ src/main/java/com/ilnitsk/animusic/album/dao/Album.java
@@ -21,7 +21,7 @@ import java.util.List;
 @ToString
 public class Album {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     @ManyToOne
