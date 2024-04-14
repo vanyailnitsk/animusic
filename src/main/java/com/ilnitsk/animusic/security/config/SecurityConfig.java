a7a33381ac90e58c0153afc75ee8c24a92d245ee
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-04-14 15:21:08 +0300

    Добавлена установка ролей при авторизации

--- src/main/java/com/ilnitsk/animusic/security/config/SecurityConfig.java
+++ src/main/java/com/ilnitsk/animusic/security/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -27,6 +28,7 @@ import java.util.Arrays;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
     private final JwtAuthorizationFilter jwtAuthorizationFilter;
 
