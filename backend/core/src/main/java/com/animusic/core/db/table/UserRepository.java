153aecf4d268e35a3a03bbc59ccaa8bfb2506142
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-08-01 21:53:13 +0300

    add method for fetching user favourite playlist

--- backend/core/src/main/java/com/animusic/core/db/table/UserRepository.java
+++ backend/core/src/main/java/com/animusic/core/db/table/UserRepository.java
@@ -2,6 +2,7 @@ package com.animusic.core.db.table;
 
 import java.util.Optional;
 
+import com.animusic.core.db.model.Playlist;
 import com.animusic.core.db.model.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.criteria.CriteriaBuilder;
@@ -13,6 +14,10 @@ import org.springframework.stereotype.Component;
 @NoRepositoryBean
 public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByEmail(String email);
+
+    static final String FAVOURITE_PLAYLIST_NAME = "Favourite tracks";
+
+    Optional<Playlist> getUserFavouritePlaylist(Integer userId);
 
     class Impl extends RepositoryBase<User, Integer> implements UserRepository {
 
@@ -28,6 +33,16 @@ public interface UserRepository extends CrudRepository<User, Integer> {
             query.select(root)
                     .where(cb.equal(root.get("email"), email));
             return getOptionalResult(entityManager.createQuery(query));
+        }
+
+        @Override
+        public Optional<Playlist> getUserFavouritePlaylist(Integer userId) {
+            var query = "SELECT p FROM Playlist p WHERE p.user.id = :userId AND p.name = :name";
+            return getOptionalResult(
+                    entityManager.createQuery(query, Playlist.class)
+                            .setParameter("userId", userId)
+                            .setParameter("name", FAVOURITE_PLAYLIST_NAME)
+            );
         }
     }
 }
--- backend/core/src/test/java/com/animusic/core/db/table/UserRepositoryTest.java
+++ backend/core/src/test/java/com/animusic/core/db/table/UserRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.animusic.core.db.table;
 
 import com.animusic.core.db.DatabaseTest;
+import com.animusic.core.db.model.Playlist;
 import com.animusic.core.db.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,9 @@ class UserRepositoryTest extends DatabaseTest {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    PlaylistRepository playlistRepository;
+
     @Test
     void findByEmail() {
         assertThat(userRepository.findByEmail("email")).isEmpty();
@@ -24,5 +28,37 @@ class UserRepositoryTest extends DatabaseTest {
                 .build();
         userRepository.save(user);
         assertThat(userRepository.findByEmail("email").get()).isEqualTo(user);
+    }
+
+    @Test
+    void getUserFavouritePlaylist() {
+        var user = User.builder()
+                .email("email")
+                .username("username")
+                .password("password")
+                .build();
+        userRepository.save(user);
+        var playlist = Playlist.builder()
+                .name("Favourite tracks")
+                .user(user)
+                .build();
+        playlistRepository.save(playlist);
+        assertThat(userRepository.getUserFavouritePlaylist(user.getId()).get()).isEqualTo(playlist);
+    }
+
+    @Test
+    void getUserFavouritePlaylistNotFound() {
+        var user = User.builder()
+                .email("email")
+                .username("username")
+                .password("password")
+                .build();
+        userRepository.save(user);
+        var playlist = Playlist.builder()
+                .name("My playlist")
+                .user(user)
+                .build();
+        playlistRepository.save(playlist);
+        assertThat(userRepository.getUserFavouritePlaylist(user.getId())).isEmpty();
     }
 }
