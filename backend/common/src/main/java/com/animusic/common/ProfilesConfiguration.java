e767181c6de8b90c20dfc36d289f6513cbc1459a
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-08-02 17:48:40 +0300

    configuration for using application-#profile.properties

---
+++ backend/common/src/main/java/com/animusic/common/ProfilesConfiguration.java
@@ -0,0 +1,10 @@
+package com.animusic.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application-${spring.profiles.active}.properties")
+public class ProfilesConfiguration {
+    
+}
