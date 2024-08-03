6c43e874f5790885c65099d09599a5cf8c507d07
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-08-03 15:03:41 +0300

    Docker for postgres is needed in `dev` profile so tests will use `zonky-embedded-postgres`

--- backend/core/src/test/java/com/animusic/core/db/DatabaseTest.java
+++ backend/core/src/test/java/com/animusic/core/db/DatabaseTest.java
@@ -1,11 +1,15 @@
 package com.animusic.core.db;
 
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseProvider.ZONKY;
 
 @SpringJUnitConfig({
         TestingDbConfig.class
 })
+@AutoConfigureEmbeddedDatabase(provider = ZONKY)
 @Slf4j
 public class DatabaseTest {
 
