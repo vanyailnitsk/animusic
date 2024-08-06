9205e07b8d6e73db618c50ed51147f65385e2ff4
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-08-06 12:22:50 +0300

    prepare main

---
+++ frontend/src/components/MediaLibrary/MediaLibrary.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './MediaLibrary.css'
+import FavoriteTracks from "../UserPlaylist/FavoriteTracks";
+
+interface MediaLibraryProps{
+    menuActive:boolean
+}
+const MediaLibrary = ({menuActive} :MediaLibraryProps) => {
+
+    return (
+        <div className={menuActive? 'media__library__wrapper menu__active' : 'media__library__wrapper'}>
+            <FavoriteTracks/>
+        </div>
+    );
+};
+
+export default MediaLibrary;
---
+++ frontend/src/pages/SearchPage/SearchPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../SearchPage/SearchPage.css'
+
+const SearchPage = () => {
+    return (
+        <div className='search__page__wrapper'>
+
+        </div>
+    );
+};
+
+export default SearchPage;
