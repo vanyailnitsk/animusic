f542ef1cb0c7e449517001a6635b7b48d42971cf
kolyailnitsk <kolya.ilnitsk@yandex.ru>
2024-08-08 18:15:10 +0300

    resolve conflicts

---
+++ src/entities/anime/index.ts
@@ -0,0 +1,4 @@
+export type {IAnime} from './model/anime'
+export {AnimeCard} from './ui/anime-card/animeCard'
+export {getAnimeInfo} from './api/getAnimeInfo.ts'
+export {getAllAnime} from './api/getAllAnime.ts'
