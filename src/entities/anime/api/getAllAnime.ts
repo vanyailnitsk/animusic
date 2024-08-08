897f214d15b71c9b30dc264f3f3354cee1621c64
kolyailnitsk <kolya.ilnitsk@yandex.ru>
2024-08-08 18:19:40 +0300

    resolve conflicts

--- src/entities/anime/api/getAllAnime.ts
+++
@@ -1,8 +0,0 @@
-import {$host} from "@/shared/api";
-import {AxiosResponse} from "axios";
-import {IAnime} from "@/entities/anime";
-
-export const getAllAnime = async (): Promise<AxiosResponse<IAnime[]>> => {
-    const response: AxiosResponse<IAnime[]> = await $host.get('anime');
-    return response;
-}
