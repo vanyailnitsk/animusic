f335eb02d2c653739b37844b55bfe1a1174fa88d
kolyailnitsk <kolya.ilnitsk@yandex.ru>
2024-08-08 18:14:24 +0300

    resolve conflicts

---
+++ src/shared/lib/fetching/useFetching.tsx
@@ -0,0 +1,34 @@
+import {useEffect, useState} from "react";
+import {AxiosResponse} from "axios";
+
+export const useFetching =
+<
+T > (requestCallback
+:
+() => Promise<AxiosResponse<T>>, deps
+:
+any[] = []
+) =>
+{
+    const [data, setData] = useState<null | T>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string>('');
+
+    useEffect(() => {
+        const fetching = async () => {
+            setIsLoading(true);
+            try {
+                const response = await requestCallback();
+                setData(response.data);
+            } catch (e: any) {
+                setError(e.message);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        fetching();
+    }, deps);
+
+    return {data, isLoading, error};
+}
+;
