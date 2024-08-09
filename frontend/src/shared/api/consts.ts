e9c17e5fd4694b146cb60bf5099c9ca149e98836
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-08-09 17:33:01 +0300

    change s3-url

--- frontend/src/shared/api/consts.ts
+++ frontend/src/shared/api/consts.ts
@@ -1,6 +1,4 @@
-
-// export const storageUrl = "https://storage.yandexcloud.net/animusic-s3/"
-export const storageUrl = "https://s3.timeweb.cloud/7d46835a-animusic-storage/"
+export const storageUrl = "https://storage.yandexcloud.net/animusic-s3/"
 export const collection = 'collection'
 
 
