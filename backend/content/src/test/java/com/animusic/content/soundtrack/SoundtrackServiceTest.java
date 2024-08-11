a8e74d56898789e09f628201cc86fbaebd2b394c
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-08-11 14:10:34 +0300

    test fix

--- backend/content/src/test/java/com/animusic/content/soundtrack/SoundtrackServiceTest.java
+++ backend/content/src/test/java/com/animusic/content/soundtrack/SoundtrackServiceTest.java
@@ -1,36 +1,34 @@
 package com.animusic.content.soundtrack;
 
 import com.animusic.content.IntegrationTestBase;
+import com.animusic.core.db.model.Album;
+import com.animusic.core.db.model.Anime;
 import com.animusic.core.db.model.Soundtrack;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Transactional
-@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
 class SoundtrackServiceTest extends IntegrationTestBase {
 
     @Autowired
     SoundtrackService soundtrackService;
 
     @Test
-    void getSoundtrackNotFound() {
-        assertThatThrownBy(() -> soundtrackService.getSoundtrack(1123))
-                .isInstanceOf(SoundtrackNotFoundException.class);
-    }
-
-    @Test
-    void getSoundtrack() {
-        assertThat(soundtrackService.getSoundtrack(1)).isNotNull();
-    }
-
-    @Test
     void createSoundtrack() {
+        var anime = Anime.builder()
+                .title("Anime-1")
+                .folderName("Anime1")
+                .build();
+        animeRepository.save(anime);
+        var album = Album.builder()
+                .name("Openings")
+                .anime(anime)
+                .build();
+        albumRepository.save(album);
         var audio = new MockMultipartFile("audio", "track.aac", "audio/aac", "audio".getBytes());
         var image = new MockMultipartFile("image", "logo.png", "img/png", "image".getBytes());
         var soundtrack = Soundtrack.builder()
@@ -38,9 +36,10 @@ class SoundtrackServiceTest extends IntegrationTestBase {
                 .originalTitle("Song")
                 .duration(123)
                 .build();
-        soundtrackService.createSoundtrack(audio, image, soundtrack, 1);
+        soundtrackService.createSoundtrack(audio, image, soundtrack, album.getId());
         assertThat(soundtrackRepository.findById(soundtrack.getId()).get())
                 .isEqualTo(soundtrack);
+
         var expectedAudioFile = "Anime1/audio/Opening 1.aac";
         assertThat(s3Service.getObject(expectedAudioFile)).isEqualTo("audio".getBytes());
         assertThat(soundtrack.getAudioFile()).isEqualTo(expectedAudioFile);
@@ -50,9 +49,6 @@ class SoundtrackServiceTest extends IntegrationTestBase {
         assertThat(soundtrack.getImage().getSource()).isEqualTo(expectedImage);
     }
 
-    @Test
-    void setImage() {
-    }
 
     @Test
     void remove() {
