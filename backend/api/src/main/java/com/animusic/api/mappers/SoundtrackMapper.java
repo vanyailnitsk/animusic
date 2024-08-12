ad8f51133adc36f27b7907731501eca86cbce96d
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-08-12 13:23:16 +0300

    return default soundtrack image

--- backend/api/src/main/java/com/animusic/api/mappers/SoundtrackMapper.java
+++ backend/api/src/main/java/com/animusic/api/mappers/SoundtrackMapper.java
@@ -1,11 +1,13 @@
 package com.animusic.api.mappers;
 
 import java.util.List;
+import java.util.Objects;
 
 import com.animusic.api.dto.PlaylistSoundtrackDto;
 import com.animusic.api.dto.SoundtrackDto;
 import com.animusic.api.dto.SoundtrackEntityDto;
 import com.animusic.content.soundtrack.SoundtrackSavedHelper;
+import com.animusic.core.db.model.Image;
 import com.animusic.core.db.model.PlaylistSoundtrack;
 import com.animusic.core.db.model.Soundtrack;
 import com.animusic.s3.StoragePathResolver;
@@ -19,12 +21,13 @@ public class SoundtrackMapper {
     }
 
     public static SoundtrackEntityDto soundtrackEntity(Soundtrack soundtrack) {
+        var image = Objects.requireNonNullElse(soundtrack.getImage(), defaultSoundtrackImage());
         return new SoundtrackEntityDto(
                 soundtrack.getId(),
                 soundtrack.getOriginalTitle(),
                 soundtrack.getAnimeTitle(),
                 StoragePathResolver.getAbsoluteFileUrl(soundtrack.getAudioFile()),
-                ImageMapper.fromImage(soundtrack.getImage()),
+                ImageMapper.fromImage(image),
                 soundtrack.getDuration(),
                 SoundtrackSavedHelper.isSaved(soundtrack),
                 AlbumMapper.albumItemDto(soundtrack.getAlbum()),
@@ -44,5 +47,9 @@ public class SoundtrackMapper {
         return playlistSoundtracks.stream()
                 .map(ps -> new PlaylistSoundtrackDto(ps.getAddedAt(), soundtrackEntity(ps.getSoundtrack())))
                 .toList();
+    }
+
+    private static Image defaultSoundtrackImage() {
+        return Image.builder().source("images/track-img.jpeg").build();
     }
 }
