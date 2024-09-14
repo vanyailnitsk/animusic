aca6ce36f95e1b7f7e2cc8895ed52695dbf14ec7
Ilnitskiy Ivan <vanyailnitsk@yandex.ru>
2024-09-14 12:01:16 +0300

    create tables: - subscription_for_album - subscription_for_anime

---
+++ backend/core/src/main/java/com/animusic/core/db/model/SubscriptionForAnime.java
@@ -0,0 +1,42 @@
+package com.animusic.core.db.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "subscription_for_anime")
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubscriptionForAnime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "anime_id", nullable = false)
+    private Anime anime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "added_at")
+    private Date addedAt;
+
+}
